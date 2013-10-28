5c584d5a8cd7740372fc49bb183ceb70d03510c8
Sankha Narayan Guria <sankha93@gmail.com>
2013-10-28 22:09:11 +0530

    created morse header

--- morse.c
+++ morse.c
@@ -1,30 +1,4 @@
-#include "stack.h"
-#include "tokens.h"
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <math.h>
-#include <stddef.h>
-
-#define ERR_FILE_OPEN "Error: Unable to openfile.\n"
-#define ERR_FILE_UNSPECIFIED "Error! No input file specified.\n"
-#define ERR_LINE_NOT_FOUND "Error: Line not found.\n"
-#define ERR_FILE_OPEN_CODE 1
-#define ERR_LINE_NOT_FOUND_CODE 2
-
-size_t lineLen = 0;
-ssize_t read;
-FILE *fp;
-int lineNum;
-
-char *readLine();
-void openFile(char*);
-void parseTokens(char*);
-char *str_replace(char*, char*, char*);
-int parseInt(char*);
-int binary_decimal(int);
-void jumpLine(char*);
+#include "morse.h"
 
 int main(int argc, char *argv[]) {
     if (argc <= 1) {
